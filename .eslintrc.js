ddfff6afcfc5a6fb7d3f543dfd31e4232658df89
C3NZ <cenz@cenz.io>
2018-12-04 15:42:33 -0800

    [feat] add eslint for linting js code

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "env": {
+        "browser": true,
+        "node": true,
+        "mocha": true,
+    },
+    "rules": {
+        "indent": [2, 4],
+    }
+};
